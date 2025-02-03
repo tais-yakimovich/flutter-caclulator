5f133f1877ea7a9d28583b42937240d3ba395162
Coding Rooms <git@codingrooms.io>
2025-02-03 21:40:41 +0000

    Initial commit of the Flutter calculator app

---
+++ lib/calculator_button.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class calcButton extends StatelessWidget {
+  final String value;
+  final Function(String) onPressed;
+
+  calcButton({required this.value, required this.onPressed});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: ElevatedButton(
+        onPressed: () => onPressed(value),
+        style: ElevatedButton.styleFrom(
+          backgroundColor: value == 'C' ? Colors.amber[500] : Colors.brown[300],
+          shape: CircleBorder(),
+          padding: EdgeInsets.all(27.0),
+          
+        ),
+        child: Text(
+          value,
+          style: TextStyle(
+            fontSize: 24, 
+            fontWeight: FontWeight.bold, 
+            color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:flutter/material.dart';
+import 'package:expressions/expressions.dart'; // External package for expression evaluation
+import 'calculator_button.dart';
+
+void main() {
+  runApp(CalculatorApp());
+}
+
+class CalculatorApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: CalculatorScreen(),
+    );
+  }
+}
+
+class CalculatorScreen extends StatefulWidget {
+  @override
+  _CalculatorScreenState createState() => _CalculatorScreenState();
+}
+
+class _CalculatorScreenState extends State<CalculatorScreen> {
+  String _expression = '';
+  String _result = '';
+
+  void _onPressed(String value) {
+    setState(() {
+      if (value == '=') {
+        try {
+          final expression = Expression.parse(_expression);
+          final evaluator = const ExpressionEvaluator();
+          _result = evaluator.eval(expression, {}).toString();
+          _expression += ' = $_result';
+        } catch (e) {
+          _result = 'Error';
+        }
+      } else if (value == 'C') {
+        _expression = '';
+        _result = '';
+      } else {
+        _expression += value;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Calculator Taisa Yakimovich',
+        style: TextStyle(
+          color: Colors.white,
+        ), ),
+        backgroundColor: Colors.brown[500],
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Container(
+              alignment: Alignment.bottomRight,
+              padding: EdgeInsets.all(16),
+              child: Text(
+                _expression,
+                style: TextStyle(
+                  fontSize: 32, 
+                  fontWeight: FontWeight.bold),
+                textAlign: TextAlign.right,
+              ),
+            ),
+          ),
+          Divider(),
+          Expanded(
+            flex: 1,
+            child: Column(
+              children: [
+                _buildRow(['7', '8', '9', '/']),
+                _buildRow(['4', '5', '6', '*']),
+                _buildRow(['1', '2', '3', '-']),
+                _buildRow(['C', '0', '=', '+']),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildRow(List<String> values) {
+    return Row(
+      children: values
+          .map((value) => calcButton(value: value, onPressed: _onPressed))
+          .toList(),
+    );
+  }
+}
